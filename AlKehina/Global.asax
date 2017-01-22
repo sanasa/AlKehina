85450890caf1ce1e4d4fd1c61ea59f76556e8856
sana <khiarisanaa@gmail.com>
2017-01-22 13:28:17 +0100

    first

---
+++ AlKehina/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlKehina.MvcApplication" Language="C#" %>
---
+++ AlKehina/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlKehina.Startup))]
+namespace AlKehina
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
